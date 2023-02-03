6cc1f6a0e2a4e267ffd0407cd38ee35274afceef
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 20:30:28 +0430

    Fizzbuzz solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,15 @@
 module.exports = function () {
-  // write your code here
+  let result = [];
+  for (let i = 1; i <= 100; i++){
+    if (i % 5 === 0 && i % 3 === 0){
+      result.push('FizzBuzz')
+    } else if (i % 3 === 0){
+      result.push('Fizz')
+    } else if (i % 5 === 0){
+      result.push('Buzz')
+    } else {
+      result.push(i)
+    }
+  }
+  return result
 }
